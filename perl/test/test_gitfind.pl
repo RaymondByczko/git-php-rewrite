8c49f15a35efe15e68b6b535f803d61db46cef36
Raymond Byczko <raymondbyczko@att.net>
2016-04-13 13:54:34 -0400

    RByczko;2016-04-13 April 13,2016;Added env var support for test_gitfind.pl

--- perl/test/test_gitfind.pl
+++ perl/test/test_gitfind.pl
@@ -5,24 +5,26 @@
 # @start_date 2016-04-11 April 10, 2016
 # @purpose To provide support in reading from a git history.
 # @change_history 2016-04-10, RByczko, Started this file.
+# @change_history 2016-04-12, RByczko, Env var support.
 
 use strict;
 use Modern::Perl;
 use Getopt::Long;
 use Log::Log4perl qw(get_logger);
+use Env qw(GITSEARCH_HOME GITSEARCH_LIB);
 
-use lib('/home/raymond/websites/remindme/perl/lib');
+use lib($GITSEARCH_LIB);
 
 use gitsearch;
 
-Log::Log4perl->init("./test_gitfind.conf");
+Log::Log4perl->init($GITSEARCH_HOME."/test_gitfind.conf");
 
 my $githash = '';
 GetOptions("githash=s"=>\$githash)
 or die('Error in command line options');
 
 if (!($githash)) {
-    die("Error in command line-all parameters required");
+    die("Error in command line-all parameters required. Usage: --githash HHHH..");
 }
 
 Log::Log4perl->init("./test_gitfind.conf");
