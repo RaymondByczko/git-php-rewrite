38235c44c178c1278db83e5cc889d598ddb53be9
Raymond Byczko <raymondbyczko@att.net>
2016-04-13 13:05:04 -0400

    RByczko;2016-04-13 April 13, 16; Added debug echo and fixed.

--- document_root/php-bin/gitshamin.php
+++ document_root/php-bin/gitshamin.php
@@ -7,20 +7,30 @@
   * This is a copy/modification gitsha1.php
   * @purpose To produce sha1 values for a certain file that match
   * that produced by git.
-  * @change_history Changed extension from gitsha1 to gitshamin.
+  * @change_history 2016-04-13, RByczko, Changed extension from gitsha1 to gitshamin.
+  * @change_history 2016-04-13, RByczko, Debug echo utilized.
   */
 $pt = $_SERVER['REQUEST_URI'];
 $dr = $_SERVER['DOCUMENT_ROOT'];
+echo '<pre>'.'pt='.$pt.'</pre>';
+echo '<pre>'.'dr='.$dr.'</pre>';
 $posSuffixStart = strpos($pt, '.gitshamin');
+echo '<pre>'.'posSuffixStart='.$posSuffixStart.'</pre>';
 $realFile = substr($pt, 1, $posSuffixStart-1);
-$fe = file_exists($dr.'/'.$realFile);
+echo '<pre>'.'realFile='.$realFile.'</pre>';
+
+$realPN = $dr.'/'.$realFile;
+echo '<pre>'.'realPN='.$realPN.'</pre>';
+// $fe = file_exists($dr.'/'.$realFile);
+$fe = file_exists($realPN);
 if ($fe == false)
 {
 	echo '<p>';
 	echo 'This file does not exist: '.$dr.'/'.$realFile;
 	return;
 }
-$filecontents = file_get_contents($realFile, FILE_USE_INCLUDE_PATH);
+// $filecontents = file_get_contents($realPN, FILE_USE_INCLUDE_PATH);
+$filecontents = file_get_contents($realPN);
 $filesize = strlen($filecontents);
 $gitentity = 'blob '.$filesize."\000".$filecontents;
 $sha1ge = sha1($gitentity);
